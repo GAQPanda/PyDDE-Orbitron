5035541f5443fe99d1154737b618245549ad25e5
Fat Panda <70119893+GAQPanda@users.noreply.github.com>
2024-02-16 20:48:35 +0800

    Update Main Src
    Update Main Src

--- DDE.py
+++ DDE.py
@@ -19,7 +19,7 @@ while True:
     try:
         conversation.ConnectTo("Orbitron", "Tracking")
         break
-    except:
+    finally:
         print("未检测到Orbitron进程，请确保Orbitron已经启动并打开DDE服务")
         input("请确保Orbitron已经启动并打开DDE服务，短按Enter键进行重新连接")
 
@@ -34,12 +34,33 @@ while True:
         os.system('cls')
         lst = data.split()
         # 打印接收到的数据
-        print("卫星名称：", lst[0])
-        print("卫星方位角：", lst[1])
-        print("卫星仰角：", lst[2])
-        print("下行多普勒频率：", lst[3])
-        print("上行多普勒频率：", lst[4])
-        print("下行解调模式：", lst[5])
-        print("上行调制模式：", lst[6])
+        try:
+            print("卫星名称：", lst[0].lstrip("SN"))
+        except IndexError:
+            print("卫星名称：", "未知")
+        try:
+            print("卫星方位角：", lst[1].lstrip("AZ"))
+        except IndexError:
+            print("卫星方位角：", "未知")
+        try:
+            print("卫星仰角：", lst[2].lstrip("EL"))
+        except IndexError:
+            print("卫星仰角：", "未知")
+        try:
+            print("下行多普勒频率：", lst[3].lstrip("DN"))
+        except IndexError:
+            print("下行多普勒频率：", "未知")
+        try:
+            print("上行多普勒频率：", lst[4].lstrip("UP"))
+        except IndexError:
+            print("上行多普勒频率：", "未知")
+        try:
+            print("下行解调模式：", lst[5].lstrip("MDN"))
+        except IndexError:
+            print("下行解调模式：", "未知")
+        try:
+            print("上行调制模式：", lst[6].lstrip("MUP"))
+        except IndexError:
+            print("上行调制模式：", "未知")
         # 等待1秒
         time.sleep(1)
